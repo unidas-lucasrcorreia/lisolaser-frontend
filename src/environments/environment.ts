90dfbae41df66aa1ef21b45244bc93f016bc1c08
Danilo Zangari <danilo.zangari@venicetech.com.br>
2025-10-30 15:45:34 -0300

    env provisorio

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://t0ow00kc0s8g0gkk4koc0w4s.72.60.248.200.sslip.io/api',
+  // apiUrl: 'https://t0ow00kc0s8g0gkk4koc0w4s.72.60.248.200.sslip.io/api',
+    apiUrl: 'https://lisolaser-api-hfd3cah5ccafa4hc.brazilsouth-01.azurewebsites.net/api',
 };
