852783c0d458f985ada7816f628e8c5a2db8f0af
Danilo Zangari <danilo.zangari@venicetech.com.br>
2025-10-30 17:46:06 -0300

    Nova ApiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://t0ow00kc0s8g0gkk4koc0w4s.72.60.248.200.sslip.io/api',
-    apiUrl: 'https://lisolaser-api-hfd3cah5ccafa4hc.brazilsouth-01.azurewebsites.net/api',
+  apiUrl: 'https://lisolaser.com.br/backend/api',
 };
